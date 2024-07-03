18506de5a13065e5451978d79f20680469d1aef2
saikrish1105 <krishsaivishal@gmail.com>
2024-07-03 13:27:50 +0530

    6th Commit

--- thumbnail.py
+++ thumbnail.py
@@ -1,4 +1,4 @@
-# my first change lmao
+# All imports
 import google.generativeai as genai
 from IPython.display import Markdown,display
 from googleapiclient.discovery import build
@@ -6,23 +6,15 @@ from googleapiclient.errors import HttpError
 from pytube import YouTube
 import re
 import urllib.request
-from youtube_extraction import url, video_id, video_title
+from diffusers import StableDiffusionPipeline
+import torch
+import matplotlib.pyplot as plt
+from youtube_extraction import url, video_id
 
 # API Key
 GOOGLE_API_KEY = 'AIzaSyBPvhyyElfd-l2-tg1xDwVDSQ-7DQgNAvg'
 genai.configure(api_key=GOOGLE_API_KEY)
 
-
-# Extract video ID from YouTube URL
-def extract_video_id(url):
-    pattern = r'(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})'
-    match = re.search(pattern, url)
-    return match.group(1) if match else None
-
-#video url for saving file
-video_url = input("Enter YouTube link: ")
-#video id for getting video title
-video_id = extract_video_id(video_url)
 
 def get_video_title(video_idd):
     # Fetch video title using YouTube Data API
@@ -55,31 +47,46 @@ def save_thumbnail(link,name_to_save):
   print(f"** {name_to_save} Saved!! **")
 
 def generate_from_thumb(thumbnail_name,script,prompt):
-   thumb = genai.upload_file(path=f"{thumbnail_name}.jpg")
-   print(f"Uploaded file '{thumb.display_name}' as: {thumb.uri}")
-   
-   # combine script into the prompt
-   combined_prompt = f"{prompt}\n\nHere is the script for the video:\n{script}"
-   # Choose a Gemini API model.
-   model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest")
-    
-   # Prompt the model with text and the previously uploaded image.
-   response = model.generate_content([thumb, combined_prompt])
-   
-   # markdown does not work in vscode and works only n jupter format
-   # display(Markdown(">" + response.text))
-   return response.text
+    thumb = genai.upload_file(path=f"{thumbnail_name}.jpg")
+    print(f"Uploaded file '{thumb.display_name}' as: {thumb.uri}")
+    # Choose a Gemini API model.
+    model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest")
+     
+    # create two different prompts- one for text gen - another to send to image gen
+    text_prompt = f"{prompt}\n\nHere is the script for the video:\n{script}"
+    image_prompt = f"""Give me just one prompt for an AI so that I can make an image 
+    similar to the image and is suitable for my script 
+    \n\nHere is the script for the video:\n{script}"""
 
+    # Prompt the model with text and the previously uploaded image.
+    text_response = model.generate_content([thumb, text_prompt])
+    image_response = model.generate_content([thumb,image_prompt])
+
+    #send image response as a new prompt to image gen
+    image = generate_image(image)
+    
+    # markdown does not work in vscode and works only n jupter format
+    # display(Markdown(">" + response.text))
+    return image,text_response.text
+    
+def generate_image(prompt):
+  image_gen_model = "stabilityai/stable-diffusion-xl-base-1.0"
+  pipe = StableDiffusionPipeline.from_pretrained(image_gen_model, torch_dtype=torch.float16, use_safetensors=True)
+  pipe = pipe.to("cuda")
+  image = pipe(prompt).images[0]
+  print("[PROMPT]: ",prompt)
+  
 
 
 video_title = get_video_title(video_id)
-save_thumbnail(video_url,video_title)
+save_thumbnail(url,video_title)
 
 script = str(input("Please enter the script  "))
 prompt = str(input("Please enter the prompt  "))
 
 
-
-answer = generate_from_thumb(video_title,script,prompt)
+image,answer = generate_from_thumb(video_title,script,prompt)
 
 print(" Answer :\n",answer)
+plt.imshow(image);
+plt.axis('off');
